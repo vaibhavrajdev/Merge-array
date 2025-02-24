fa626f30efd61f1899cf0dcf40a793a0324cd97f
Vaibhav Raj <37078821+vaibhavrajdev@users.noreply.github.com>
2025-02-25 00:55:50 +0530

    Optimized the sorting.

--- merge-array.component.ts
+++ merge-array.component.ts
@@ -1,43 +1,67 @@
-
 export class MergeArrayComponent {
-    callApiMethod(arr1: number[], arr2: number[]) {
-      const mergedArray = this.mergeAndSortArrays(arr1, arr2);
-      console.log('merged and sorted', mergedArray);
-      console.log('merged');
+  smallSorting: number = 30;
+
+  callApiMethod(arr1: number[], arr2: number[]) {
+    const mergedArray = this.mergeAndSortArrays(arr1, arr2);
+    console.log('Merged and Sorted:', mergedArray);
+  }
+
+  mergeAndSortArrays(arr1: number[], arr2: number[]): number[] {
+    const mergedArr = arr1.concat(arr2);
+    return this.timSort(mergedArr);
+  }
+
+  timSort(arr: number[]): number[] {
+    const n = arr.length;
+    
+    for (let i = 0; i < n; i += this.smallSorting) {
+      this.insertionSort(arr, i, Math.min(i + this.smallSorting - 1, n - 1));
     }
-  
-    mergeAndSortArrays(arr1: number[], arr2: number[]): number[] {
-      const mergedArr = [...arr1, ...arr2];
-      return this.quickSort(mergedArr);
-    }
-  
-    quickSort(arr: number[]): number[] {
-      if (arr.length <= 1) {
-        return arr;
-      }
-  
-      const point = arr[Math.floor(arr.length / 2)];
-      const left: number[] = [];
-      const right: number[] = [];
-      const equal: number[] = [];
-  
-      for (let num of arr) {
-        if (num < point) {
-          left.push(num);
-        } else if (num > point) {
-          right.push(num);
-        } else {
-          equal.push(num);
+
+    let size = this.smallSorting;
+    while (size < n) {
+      for (let left = 0; left < n; left += 2 * size) {
+        const mid = left + size - 1;
+        const right = Math.min(left + 2 * size - 1, n - 1);
+        if (mid < right) {
+          this.merge(arr, left, mid, right);
         }
       }
-  
-      return [...this.quickSort(left), ...equal, ...this.quickSort(right)];
+      size *= 2;
+    }
+
+    return arr;
+  }
+
+  private insertionSort(arr: number[], left: number, right: number) {
+    for (let i = left + 1; i <= right; i++) {
+      let temp = arr[i];
+      let j = i - 1;
+      while (j >= left && arr[j] > temp) {
+        arr[j + 1] = arr[j];
+        j--;
+      }
+      arr[j + 1] = temp;
     }
   }
-  
-  const arrData1 = [12, 3, 9, 1];
-  const arrData2 = [7, 10, 2, 8];
-  
-  const apicall = new MergeArrayComponent();
-  apicall.callApiMethod(arrData1, arrData2);
-  
+
+  private merge(arr: number[], left: number, mid: number, right: number) {
+    const leftArr = arr.slice(left, mid + 1);
+    const rightArr = arr.slice(mid + 1, right + 1);
+    
+    let i = 0, j = 0, k = left;
+
+    while (i < leftArr.length && j < rightArr.length) {
+      arr[k++] = leftArr[i] <= rightArr[j] ? leftArr[i++] : rightArr[j++];
+    }
+    
+    while (i < leftArr.length) arr[k++] = leftArr[i++];
+    while (j < rightArr.length) arr[k++] = rightArr[j++];
+  }
+}
+
+const arr1 = [12, 3, 9, 1];
+const arr2 = [7, 10, 2, 8];
+
+const apiCall = new MergeArrayComponent();
+apiCall.callApiMethod(arr1, arr2);
